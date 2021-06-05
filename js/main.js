fe12ede94067cc63da64800757ce1e5b268c10dd
ildebr <73916430+ildebr@users.noreply.github.com>
2021-06-04 21:13:38 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-$("div.barras").click(function(){
-	$("svg#barra-navegacion").toggleClass("oculto");
-	$("svg#barra-navegacion-x").toggleClass("mostrar");
-	$("body").toggleClass("overf");
-	$("div.links").toggleClass("mostrar");
+$(document).ready(function(){
+    console.log("hola");
 
-	/*
-	$("div.links").css("top", function(){
-		return $(window).scrollTop();
-	})
-
-	*/
+    $("main.landing-view").addClass("decrease");
+    $("span.s-volteada").addClass("s-normal");
 })
-
-$("div.miembro").each(function(){
-     var i = $(this).prop("id");
-
-     $("div.miembro#" + i + ">div.imagen").css("background-image", "url(img/" + i + ".jpg)");
-console.log(i);});
